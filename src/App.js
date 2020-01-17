6e2d6aa2725de15cadf5fb564428e18f7fd8c47e
Tegos <tegosiv@gmail.com>
2020-01-17 11:04:01 +0200

    add redux, create store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import './App.css';
 import Counter from './components/Counter/Counter';
+import store from './store';
 
 function App() {
 	return (
-		<>
+		<Provider store={store}>
 			<Counter/>
-		</>
+		</Provider>
 	);
 }
 
